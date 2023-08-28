58f9a2df7635a75ec2d7b348f37cc262531ff2b6
Ant Gardiner <antokne@gmail.com>
2023-08-28 14:45:08 +1200

    Initial Commit

---
+++ HWMTimers/HWMTimersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWMTimersApp.swift
+//  HWMTimers
+//
+//  Created by Ant Gardiner on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct HWMTimersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
